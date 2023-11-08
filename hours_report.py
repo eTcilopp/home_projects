252bf391a9752cd1fc26e90ad0fca44b7a8c1556
eTcilopp <kirikeza@gmail.com>
2023-11-07 18:49:12 -0700

    script works

--- hours_report.py
+++ hours_report.py
@@ -1,42 +1,102 @@
 import requests
+from urllib.parse import urljoin
 import pandas as pd
 
-# The API endpoint URL
-url = 'https://internal.api.vitruvi.cc/api/v1/production/production_reports/'
-params = {
-    'created_by': 58,
-    'created_from': '2023-07-23'
-}
+USER_PROFILE_ID = 58
+API_TOKEN = '7M8AE85Ko65VoqR5P1kEnoch0rIwk3AxEsorWNmt'
+
+BASE_URL = 'https://internal.api.vitruvi.cc'
+PODUCTION_REPORT_URL = 'api/v1/raw/production/productionreport'
+PODUCTION_REPORT_LINE_URL = 'api/v1/raw/production/productionreportline'
+WORK_ITEM_URL = 'api/v1/raw/wbs/workitem'
+WORK_ITEM_QUANTITY_URL = 'api/v1/raw/wbs/workitemquantity'
+PODUCTION_REPORT_QUANTITY_URL = 'api/v1/raw/production/productionreportquantity'
+QUANTITY_DESCRIPTION_URL = 'api/v1/raw/core/quantitydescription'
+
 headers = {
     'accept': 'application/json',
-    'Authorization': 'Bearer 7M8AE85Ko65VoqR5P1kEnoch0rIwk3AxEsorWNmt'
+    'Authorization': f'Bearer {API_TOKEN}'
 }
 
-# Make the GET request
+
+
+# PR
+url = urljoin(BASE_URL, PODUCTION_REPORT_URL)
+params = {
+    'created_by_id': USER_PROFILE_ID,
+    'submitted_date__gte': '2023-10-01'
+}
+
 response = requests.get(url, params=params, headers=headers)
 pr_data = response.json()
 pr_df = pd.json_normalize(pr_data['results'])
-pr_df.set_index('id', inplace=True)
+
+pr_id_lst = set(pr_df['id'].tolist())
+pr_id_str = ','.join(map(str, pr_id_lst))
 
 
-prl_url = 'https://internal.api.vitruvi.cc/api/v1/production/production_report_lines/'
-res = list()
-for pr_id in pr_df.index.values.tolist():
-    url = prl_url + f'{pr_id}' + '/'
-    response = requests.get(url, headers=headers)
-    res.append(response.json())
-prl_df = pd.json_normalize(res)
+# PRL
+url = urljoin(BASE_URL, PODUCTION_REPORT_LINE_URL)
+
+params = {
+    'production_report_id__in': pr_id_str
+}
+response = requests.get(url, params=params, headers=headers)
+prl_data = response.json()
+prl_df = pd.json_normalize(prl_data['results'])
+
+prl_id_lst = set(prl_df['id'].tolist())
+prl_id_str = ','.join(map(str, prl_id_lst))
+
+wi_id_lst = set(prl_df['work_item_id'].tolist())
+wi_id_str = ','.join(map(str, wi_id_lst))
+
+# Work Items
+url = urljoin(BASE_URL, WORK_ITEM_URL)
+params = {
+    'id__in': wi_id_str
+}
+
+response = requests.get(url, params=params, headers=headers)
+wi_data = response.json()
+wi_df = pd.json_normalize(wi_data['results'])
 
 
-prq_ids = [id for sublist in prl_df['quantities'].dropna().tolist() for id in sublist]
-prq_url = 'https://internal.api.vitruvi.cc/api/v1/production/production_report_quantities/'
-res = list()
-for prq_id in prq_ids:
-    url = prl_url + f'{pr_id}' + '/'
-    response = requests.get(url, headers=headers)
-    res.append(response.json())
-prq_df = pd.json_normalize(res)
+
+# WIQ
+url = urljoin(BASE_URL, WORK_ITEM_QUANTITY_URL)
+
+params = {
+    'work_item_id__in': wi_id_str
+}
+response = requests.get(url, params=params, headers=headers)
+wiq_data = response.json()
+wiq_df = pd.json_normalize(wiq_data['results'])
 
 
+# PRQ
 
+url = urljoin(BASE_URL, PODUCTION_REPORT_QUANTITY_URL)
+params = {
+    'production_report_line_id__in': prl_id_str
+}
+
+response = requests.get(url, params=params, headers=headers)
+prq_data = response.json()
+prq_df = pd.json_normalize(prq_data['results'])
+
+quantity_descriotion_id_lst = set(prq_df['quantity_description_id'].tolist())
+quantity_descriotion_id_str = ','.join(map(str, quantity_descriotion_id_lst))
+
+# Quantity_description
+
+url = urljoin(BASE_URL, QUANTITY_DESCRIPTION_URL)
+
+params = {
+    'id__in': quantity_descriotion_id_str
+}
+
+response = requests.get(url, params=params, headers=headers)
+quantity_description_data = response.json()
+quantity_description_df = pd.json_normalize(quantity_description_data['results'])
 
